3070181c6aea6cf3fdf05bd4d0800f0290622475
osDimay <osipov.asm2005@asugubkin.ru>
2023-05-01 17:23:57 +0300

    feat[]: Add server and empty endpoints

---
+++ internal/user/handler.go
@@ -0,0 +1,58 @@
+package user
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"osipovPetRestApi/internal/handlers"
+)
+
+const (
+	usersUrl = "/users"
+	userUrl  = "/users/:uuid"
+)
+
+type handler struct {
+}
+
+func NewHandler() handlers.Handler {
+	return &handler{}
+}
+
+func (h *handler) Register(router *httprouter.Router) {
+	router.GET(usersUrl, h.GetList)
+	router.GET(userUrl, h.GetUserByUuid)
+	router.POST(usersUrl, h.CreateUser)
+	router.PUT(userUrl, h.UpdateUser)
+	router.PATCH(userUrl, h.PartiallyUpdateUser)
+	router.DELETE(userUrl, h.DeleteUser)
+}
+
+func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
+	w.Write([]byte("this is list of users"))
+}
+
+func (h *handler) GetUserByUuid(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
+	w.Write([]byte("this is user by uuid"))
+}
+
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(201)
+	w.Write([]byte("this is create user"))
+}
+
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("this is update user"))
+}
+
+func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("this is partially update user"))
+}
+
+func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("this is delete user"))
+}
