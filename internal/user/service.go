a57fa7c5d7ef66eca14fc1aedec7ed4ffc1eb216
osDimay <osipov.asm2005@asugubkin.ru>
2023-09-25 23:09:04 +0300

    mongodb feat[service]: Add empty service

--- internal/user/service.go
+++ internal/user/service.go
@@ -1,1 +1,16 @@
 package user
+
+import (
+	"context"
+	"osipovPetRestApi/pkg/logging"
+)
+
+type service struct {
+	storage Storage
+	logger  *logging.Logger
+}
+
+func (s *service) Create(ctx context.Context, dto Dto) (u User, err error) {
+	//TODO for next one
+	return
+}
